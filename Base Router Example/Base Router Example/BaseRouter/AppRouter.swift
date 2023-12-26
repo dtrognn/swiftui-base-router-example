b5b105f4dbfdd07d55ad48acf28c282051cfc9fd
dtrognn <dtrognn@gmail.com>
2023-12-26 14:04:12 +0700

    create AppRouter

---
+++ Base Router Example/Base Router Example/BaseRouter/AppRouter.swift
@@ -0,0 +1,52 @@
+//
+//  AppRouter.swift
+//  Base Router Example
+//
+//  Created by dtrognn on 26/12/2023.
+//
+
+import SwiftUI
+
+class AppRouter: BaseRouter<AppRouter.Screen> {
+    enum Screen: IScreen {
+        case appLaunch
+        case mainTab
+    }
+
+    @Published var screen: Screen = .appLaunch
+
+    override init() {
+        super.init()
+        self.screen = .appLaunch
+    }
+
+    deinit {
+        print("deinit:\(#file)")
+    }
+
+    private func updateScreen(isLogin: Bool) {
+        if isLogin == true {
+            self.screen = .mainTab
+        }
+        popToRootView()
+    }
+
+    override func getInstanceScreen(_ screen: Screen) -> AnyView {
+        switch screen {
+        case .appLaunch:
+            return SplashScreenView().asAnyView
+        case .mainTab:
+            return TabbarRouterView().asAnyView
+        }
+    }
+}
+
+struct AppRouterView: View {
+    @StateObject var router: AppRouter = .init()
+
+    var body: some View {
+        DGNavigationStackView(navigationPath: self.router.navigationPath) {
+            self.router.getInstanceScreen(self.router.screen)
+        }.environmentObject(self.router)
+    }
+}
