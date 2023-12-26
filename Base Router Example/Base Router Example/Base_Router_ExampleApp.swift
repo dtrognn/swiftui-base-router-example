535c5b6da48eab02b53d2f2fc3b724ed840afef1
dtrognn <dtrognn@gmail.com>
2023-12-26 11:51:23 +0700

    init project

---
+++ Base Router Example/Base Router Example/Base_Router_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Base_Router_ExampleApp.swift
+//  Base Router Example
+//
+//  Created by dtrognn on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Base_Router_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
