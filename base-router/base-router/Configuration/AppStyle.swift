1954e10c3d71b1ee1646abe8d5ba55af3095adaa
dtrognn <dtrognn@gmail.com>
2024-01-04 23:39:53 +0700

    config app

---
+++ base-router/base-router/Configuration/AppStyle.swift
@@ -0,0 +1,16 @@
+//
+//  AppStyle.swift
+//  base-router
+//
+//  Created by dtrognn on 04/01/2024.
+//
+
+import Foundation
+
+public struct AppStyle {
+    public static var font: IFont = AppFont()
+    public static var theme: ITheme = Theme()
+    public static var layout: ILayout = Layout()
+
+    public init() {}
+}
