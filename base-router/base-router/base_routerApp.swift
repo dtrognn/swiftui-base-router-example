066802886be1f6760bd26c63e9288ace623d4e8f
dtrognn <dtrognn@gmail.com>
2024-01-04 22:26:25 +0700

    init project

---
+++ base-router/base-router/base_routerApp.swift
@@ -0,0 +1,17 @@
+//
+//  base_routerApp.swift
+//  base-router
+//
+//  Created by dtrognn on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct base_routerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
