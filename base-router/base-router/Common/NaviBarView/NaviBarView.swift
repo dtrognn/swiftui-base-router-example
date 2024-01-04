0c322258c13d0b0fd460c55c481a196eeb68774b
dtrognn <dtrognn@gmail.com>
2024-01-04 23:40:11 +0700

    create navibarView

---
+++ base-router/base-router/Common/NaviBarView/NaviBarView.swift
@@ -0,0 +1,76 @@
+//
+//  NaviBarView.swift
+//  base-router
+//
+//  Created by dtrognn on 04/01/2024.
+//
+
+import Combine
+import SwiftUI
+
+public struct NaviBarView: View {
+    @EnvironmentObject private var navigationStack: DGNavigationPath
+    @ObservedObject public var screenConfiguration: ScreenConfiguration
+
+    public var body: some View {
+        VStack(spacing: AppStyle.layout.zero) {
+            HStack {
+                if screenConfiguration.showBackButton {
+                    backButton
+                }
+                Spacer()
+                titleSection
+                Spacer()
+
+                if screenConfiguration.showPopToRootButton {
+                    if navigationStack.depth >= screenConfiguration.depthScreen {
+                        homeButton
+                    } else {
+                        homeButton.opacity(0)
+                    }
+                }
+            }.padding(AppStyle.layout.standardSpace)
+                .navigationBarHidden(true)
+                .background(
+                    AppStyle.theme.naviBackgroundColor
+                        .ignoresSafeArea(edges: .top)
+                )
+            StraightLine()
+        }
+    }
+}
+
+extension NaviBarView {
+    private var backButton: some View {
+        return Button {
+            if screenConfiguration.onBackAction != nil {
+                screenConfiguration.onBackAction?()
+            } else {
+                self.navigationStack.pop()
+            }
+        } label: {
+            Image(systemName: "arrow.backward")
+        }
+    }
+
+    var titleSection: some View {
+        return VStack {
+            Text(LocalizedStringKey(screenConfiguration.title))
+                .foregroundColor(AppStyle.theme.naviTextColor)
+                .font(AppStyle.font.semibold16)
+                .lineLimit(1)
+        }
+    }
+
+    var homeButton: some View {
+        return Button {
+            navigationStack.pop(to: .root)
+        } label: { }
+    }
+}
+
+struct NaviBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NaviBarView(screenConfiguration: ScreenConfiguration(title: "Test"))
+    }
+}
