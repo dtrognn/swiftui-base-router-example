e49832dfcacfab192630bbcb0fc9749bca2f423f
dtrognn <dtrognn@gmail.com>
2024-01-04 23:17:23 +0700

    extention View

---
+++ base-router/base-router/Utils/Extension/View+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  View+Ext.swift
+//  base-router
+//
+//  Created by dtrognn on 04/01/2024.
+//
+
+import SwiftUI
+
+extension View {
+    var asAnyView: AnyView {
+        return AnyView(self)
+    }
+}
