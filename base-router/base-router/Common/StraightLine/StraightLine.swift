229f02edb880e2005267c41c438e128fbe07e8f8
dtrognn <dtrognn@gmail.com>
2024-01-04 23:28:56 +0700

    create StraightLine

---
+++ base-router/base-router/Common/StraightLine/StraightLine.swift
@@ -0,0 +1,17 @@
+//
+//  StraightLine.swift
+//  base-router
+//
+//  Created by dtrognn on 04/01/2024.
+//
+
+import SwiftUI
+
+public struct StraightLine: View {
+    public init() { }
+
+    public var body: some View {
+        Divider().frame(height: 1)
+            .foregroundColor(Color.gray)
+    }
+}
